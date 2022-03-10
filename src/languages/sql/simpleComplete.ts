3139ab42dd8ed724810942ddbc21f6dd78a1b463
guochao <463561248@qq.com>
2022-03-10 23:59:49 +0800

    ✨ feat:  添加提示

--- src/languages/sql/simpleComplete.ts
+++ src/languages/sql/simpleComplete.ts
@@ -3,8 +3,21 @@
  * @Date: 2022-03-10 21:15:03
  * @LastEditTime: 2022-03-10 21:15:04
  * @LastEditors: saber
- * @Description: 
+ * @Description:
  */
-import * as monaco from 'monaco-editor';
+import * as monaco from "monaco-editor";
 // 提示
-monaco.languages.registerCompletionItemProvider('sql', {});
+monaco.languages.registerCompletionItemProvider("sql", {
+  triggerCharacters: ["."],
+  provideCompletionItems(
+    model: monaco.editor.ITextModel,
+    position: monaco.Position,
+    context: monaco.languages.CompletionContext,
+    token: monaco.CancellationToken
+  ) {
+      return {
+        // incomplete
+        suggestions: []
+      }
+  },
+});
